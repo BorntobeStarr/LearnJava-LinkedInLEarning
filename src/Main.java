170a508d8eb2802845c99797fbfa81af87919f55
Starr <starrleer@gmail.com>
2022-06-14 18:45:26 -0400

    completed the section 2 "Building Strong Foundations" from Linked In's Learning Java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
-        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
+
+        System.out.println(studentFirstName + " " + studentLastName + " is " + studentAge +  " years old and has a GPA of " + studentGPA);
+
+        System.out.println("What do you want the students GPA to be updated to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName +  " NOW has a GPA of " + studentGPA);
+
+
+
     }
 }
