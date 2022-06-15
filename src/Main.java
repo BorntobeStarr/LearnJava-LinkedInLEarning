5d352aa5b8e068e81c83b7c30eab0a2b2f289b9e
Starr <starrleer@gmail.com>
2022-06-14 22:39:54 -0400

    completed "While Loops in Java" from section 3 "Control Flow" from Linked In's Learning Java

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
 
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
+        while (isOnRepeat){
+            System.out.println("Pleaying current song on repeat");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes.");
+            String userInput = input.next();
 
-        int inputtedNum = scanner.nextInt();
+            if(userInput.equals("yes")){
+                    isOnRepeat = false;
+            }
 
-        if (inputtedNum < 5) {
-            System.out.println("Enjoy the good luck a friend brings you");
-        } else {
-            System.out.println("Your shoe selection will make you very happy today");
+            System.out.println("playing next song");
+
         }
 
     }
