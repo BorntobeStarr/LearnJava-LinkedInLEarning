8b4ab014f9f7ee7dd3a61b60804b9e938d4e22b4
Starr <starrleer@gmail.com>
2022-06-16 15:20:53 -0400

    completed "Defining functions in Java" from section 5 "Functions in Java" from Linked In's Learning Java

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time ");
+        System.out.println("type a random word and press enter to start dev tea time ");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time ");
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Let's calculate the area of a triangle");
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Please input the base of the triangle (in inches).");
-        double base = input.nextDouble();
-
-        while (base <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
-        }
-
-        System.out.println("Please input the height of the triangle (in inches).");
-        double height = input.nextDouble();
-        while (height <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            height = input.nextDouble();
-        }
-
-        double area = (base * height) / 2;
-        System.out.println("The area is " + area);
 
     }
 }
